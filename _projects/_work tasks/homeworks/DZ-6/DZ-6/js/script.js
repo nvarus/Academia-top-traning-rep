c8fa3b3a484217fe37fe83c30bffb14daa679aeb
nvarus <varyukhin.alx@gmail.com>
2022-12-10 17:32:24 +0400

    awdfgdsfg

---
+++ _projects/_work tasks/homeworks/DZ-6/DZ-6/js/script.js
@@ -0,0 +1,13 @@
+const myArray = ['Лев', 'Волк', 'Лиса', 'Гепард'];
+// console.log(myArray);
+
+let x = 5;
+let y = 7;
+
+const func = (x, y) => { return x + y }
+
+console.log(func);
+
+console.log(func(5, 1));
+let f = func(100, 200);
+console.log(f);
