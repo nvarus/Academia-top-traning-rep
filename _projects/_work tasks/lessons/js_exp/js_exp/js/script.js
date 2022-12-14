2a51bedfb46677ebc837f6576d5cb175d6e837f7
alx <86500348+nvarus@users.noreply.github.com>
2022-12-14 20:17:39 +0400

    rfth

---
+++ _projects/_work tasks/lessons/js_exp/js_exp/js/script.js
@@ -0,0 +1,24 @@
+() => {
+	let myVar = 40;
+	console.log('hjhkjhj');
+	print('приветт');
+	print(`Привет, ${myVar}`);
+
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+
+
+}
+
+const print = (str) => {
+	console.log(str);
+}
+
+
+
+
+
+
+
+
