ee1ea2ad47f4d3f293a6033a09892a516fd94d64
alx <86500348+nvarus@users.noreply.github.com>
2023-01-05 23:53:48 +0400

    аааа
    вапва

---
+++ _projects/_work tasks/homeworks/Практические задания. Оъект. Массивы.Строки/map_reduce/dist/script.js
@@ -0,0 +1,12 @@
+"use strict";
+let userName = 'Alx';
+console.log(userName);
+class Person {
+    name;
+    age;
+    experirnce;
+    budget;
+    constructor(nm, ag, exp, bg) {
+        name = nm;
+    }
+}
---
+++ _projects/_work tasks/homeworks/Практические задания. Оъект. Массивы.Строки/map_reduce/src/script.ts
@@ -0,0 +1,16 @@
+let userName: string = 'Alx';
+console.log(userName);
+
+class Person {
+    name: string;
+    age: number;
+    experirnce: number;
+    budget: number
+    constructor(nm: string, ag, exp, bg) {
+        name = nm;
+
+    }
+
+
+
+}
