28755d219c5bbf79cd72314dedd127367214f03e
nvarus <varyukhin.alx@gmail.com>
2023-02-16 01:20:53 +0400

    ывва

--- _projects/_work tasks/homeworks/DZ-17/DZ-17_2/js/script.js
+++ _projects/_work tasks/homeworks/DZ-17/DZ-17_2/js/script.js
@@ -4,8 +4,71 @@
 дальше вводит тему занятия и отмечает присутствующих. Также добавить возможность посмотреть уже отмеченные пары.
 Хранить информацию в заранее подготовленных массивах. */
 
-class Group {
-	constructor(name, present) {
-		this.present = present
+const groups = {
+	group1: [
+		"Павлова Дарья",
+		"Гусева Алиса",
+		"Игнатов Макар",
+		"Степанова Майя",
+		"Черкасов Тимофей",
+		"Архипова Вера",
+		"Филатов Егор",
+		"Макаров Павел",
+		"Власова Мария",
+		"Прохоров Илья"
+	],
+	group2: [
+		"Новикова Анна",
+		"Ежова Анастасия",
+		"Зыкова Вера",
+		"Никитин Александр",
+		"Ефимова Мила",
+		"Титов Матвей",
+		"Алексеева Амелия",
+		"Пименова Юлия"
+	]
+}
+
+class Lesson {
+	constructor(lessonName, group, present=[], topic, saved) {
+		this.lessonName = lessonName;
+		this.group = group;
+		this.present = present;
+		this.topic = topic;
+		this.saved = saved;
 	}
 }
+
+const lessonsList = [
+	new Lesson('lesson1', groups.group1, [true, true, false, true, true, false, false, true, true, true], "Классы", true),
+	new Lesson('lesson2', groups.group2, [true, true, false, true, true, false, false, true], "Логические операторы", true),
+	new Lesson('lesson3', groups.group1, [true, true, false, true, true, false, false, true, true, true], "Циклы", true),
+]
+
+const fillOption = () => {
+	const form = document.forms.task2Form;
+	// заполняем группу
+	const selectGroup = form.task2Group;
+	Object.keys(groups).forEach(group => {
+		const option = document.createElement('option');
+		option.value = group;
+		option.textContent = group;
+		selectGroup.append(option);
+	});
+	
+	// заполняем урок
+	const selectLesson = form.task2Lesson;
+	lessonsList.forEach(lesson => {
+		const option = document.createElement('option');
+		option.value = lesson.lessonName;
+		option.textContent = lesson.lessonName;
+		selectLesson.append(option)
+	})
+	const newOption = document.createElement('option');
+	newOption.value = 'newLesson';
+	newOption.textContent = 'Новый урок';
+	selectLesson.append(newOption)
+}
+
+
+fillOption()
